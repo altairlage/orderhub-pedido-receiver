c0100524f32c54005431cf6da448e7558a895f94
Gaddini <ferreiragaddini@bol.com.br>
2025-07-22 19:25:10 -0300

    Ajustando estrutura de pastas do projeto

--- orderhub-pedido-receiver/src/main/java/com/fiap/orderhub/interfaces/IPedidoGateway.java
+++ orderhub-pedido-receiver/src/main/java/com/fiap/orderhub/interfaces/IPedidoGateway.java
@@ -1,5 +1,6 @@
 package com.fiap.orderhub.interfaces;
 
+
 import br.com.orderhub.core.dto.pedidos.CriarPedidoDTO;
 
 public interface IPedidoGateway {
---
+++ orderhub-pedido-receiver/src/test/java/com/fiap/orderhub/OrderhubPedidoReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.orderhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderhubPedidoReceiverApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
